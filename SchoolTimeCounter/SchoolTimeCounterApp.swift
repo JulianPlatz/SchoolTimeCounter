069ebac8ed699c1d9558407191969147de9b8c59
JulianPlatz <julian.platz@icloud.com>
2024-02-29 18:34:29 +0100

    Initial Commit

---
+++ SchoolTimeCounter/SchoolTimeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolTimeCounterApp.swift
+//  SchoolTimeCounter
+//
+//  Created by Julian Platz on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SchoolTimeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
